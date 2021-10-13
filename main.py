e6bc9b28bc7686abf188225458dbe1809b5b5609
Pradeep Senthil <pradeepsen99@gmail.com>
2021-10-13 12:00:16 -0500

    implemented gz compressed file conversion

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 #sample med2image command:
 #med2image -i 4week/Pig_530.nii -d out_test --reslice -t png
+#med2image -i input_nii/Pig_530.nii -d 1out_test --outputFileType png  -s -1 --reslice
 
 import os
 import gzip
 
-INPUT_FOLDER = "4week/"
+INPUT_FOLDER = "input_nii/"
+MASK_IDENTIFIER = "-mask"
 
-files_input_folder = os.listdir()
+#========= gz compressed file conversion =========#
+
 compressed_files = [f for f in os.listdir(INPUT_FOLDER) if ".gz" in f]
 
-def gunzip(source_filepath, dest_filepath, block_size=65536):
+def gz_to_nii(source_filepath, dest_filepath, block_size=65536):
     with gzip.open(source_filepath, 'rb') as s_file, \
             open(dest_filepath, 'wb') as d_file:
         while True:
@@ -18,8 +21,11 @@ def gunzip(source_filepath, dest_filepath, block_size=65536):
                 break
             else:
                 d_file.write(block)
+    print("Converted - " + source_filepath + " to " + dest_filepath)
 
-gunzip(INPUT_FOLDER + compressed_files[0], "test.nii")
 
-
-print(compressed_files)
+for i in compressed_files:
+    gz_to_nii(INPUT_FOLDER + i, INPUT_FOLDER + i[:-3] + ".nii")
+    os.remove(INPUT_FOLDER + i)
+    
+#========= med2image conversion =========#
